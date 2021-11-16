4a2b41b0fdf46987075d7768812d7d2ecd15298a
Michael Romay <amerror@gmail.com>
2021-11-15 17:29:36 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import logo from './logo.svg';
+import birbs from './tchoknwoot.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={birbs} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Hello, I am react app, welcome
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="https://duckduckgo.com"
         >
-          Learn React
+          link to search engine
+        </a>
+        <a
+          className="App-link"
+          href="https://archive.org"
+        >
+          link to archive.org
         </a>
       </header>
     </div>
